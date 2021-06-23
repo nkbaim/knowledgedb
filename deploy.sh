4a5f2393a0cf74a11c97e8b83994f84fc2a2434a
Du Yang <duyang@bjmu.edu.cn>
2021-06-23 17:54:26 +0800

    update blogs

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 rm -rf docs/.vuepress/dist/
 npm run build
 scp -r docs/.vuepress/dist/* duyang@duomics.cn:/www/server/tomcat/webapps/pkdb/
+
+
+git add -A
+git commit -m "update blogs"
+git push origin main
